14979a7cc8cdc2476b7c969c66d53bcf93177491
Apoorva <apoorva@192.168.1.10>
2021-12-27 20:23:17 +0530

    setup

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['airbnb', 'plugin:react/jsx-runtime'],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-closing-bracket-location': 'off',
+    // Add more rules here
+  },
+};
